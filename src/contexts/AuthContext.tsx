0f6257c0fe059604bef80b39d0725facf6dfd47b
andreseichi <andre_seichi@hotmail.com>
2022-07-20 16:29:54 -0300

    separano api em uma função para usar contexto do serverSide no next

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,7 +2,7 @@ import Router from 'next/router';
 import { createContext, ReactNode, useEffect, useState } from 'react';
 import { parseCookies, setCookie, destroyCookie } from 'nookies';
 
-import { api } from '../services/api';
+import { api } from '../services/apiClient';
 
 type SignInCredentials = {
   email: string;
