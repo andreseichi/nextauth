798e812240496988432311dfbdf15c5724eb02bc
andreseichi <andre_seichi@hotmail.com>
2022-07-20 21:09:52 -0300

    componente visível de acordo com permissions e roles

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect } from 'react';
+import { Can } from '../components/Can';
 import { AuthContext } from '../contexts/AuthContext';
 import { setupAPIClient } from '../services/api';
 import { api } from '../services/apiClient';
@@ -11,7 +12,15 @@ export default function Dashboard() {
 
   const { user } = useContext(AuthContext);
 
-  return <h1>Dashboard: {user?.email}</h1>;
+  return (
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      <Can permissions={['metrics.list']} roles={['editor', 'administrator']}>
+        <div>Métricas</div>
+      </Can>
+    </>
+  );
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
